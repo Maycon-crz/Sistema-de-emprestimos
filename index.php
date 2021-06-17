352016c3bd5c97f928a07a1cd584b89cc8428f71
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2021-06-17 14:57:46 -0300

    Enunciado do problema

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Desafio Serasa</title>
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">    
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">	    
+</head>
+<body>
+	<div class='container border'>
+		<div class='row'>
+			<div class='col-12'>
+				<h1 class='text-center text-secondary border'>Desafio Serasa ProWay</h1>
+			</div>			
+		</div>
+		<div class='row'>
+			
+		</div>
+	</div>
+</body>
+<footer>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</footer>
+</html>
